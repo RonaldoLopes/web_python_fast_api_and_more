9e4667915db6702c6eaf309358484ad0f323a2ed
RonaldoLopes <ronaldorclamaster@gmail.com>
2024-07-18 15:58:02 -0300

    [Montados rotas e validaçoes]

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+from fast_zero.schemas import Message, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo'}
+
+@app.post('/users/')
+def create_user(user: UserSchema):
+    ...
+
+# https://www.youtube.com/watch?v=WnhDgVLYfx0&list=PLOQgLBuj2-3IuFbt-wJw2p2NiV9WTRzIP
+# 19:40
